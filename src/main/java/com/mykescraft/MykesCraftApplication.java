d8d7e1b2be292a545684c3c215fd917f94c81f13
kafr007 <kafr007@gmail.com>
2018-07-13 20:03:22 +0200

    Initial commit

---
+++ src/main/java/com/mykescraft/MykesCraftApplication.java
@@ -0,0 +1,12 @@
+package com.mykescraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MykesCraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MykesCraftApplication.class, args);
+	}
+}
