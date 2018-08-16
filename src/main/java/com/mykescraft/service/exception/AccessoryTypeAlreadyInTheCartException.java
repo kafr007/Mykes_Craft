11391e50c243382aa58a6e36ef974070a0d16448
kafr007 <kafr007@gmail.com>
2018-08-16 22:16:48 +0200

    add AccessoryTypeAlreadyInTheCart exception

---
+++ src/main/java/com/mykescraft/service/exception/AccessoryTypeAlreadyInTheCartException.java
@@ -0,0 +1,9 @@
+package com.mykescraft.service.exception;
+
+public class AccessoryTypeAlreadyInTheCartException extends Exception {
+	
+	public AccessoryTypeAlreadyInTheCartException(String message) {
+		super(message);
+	}
+
+}
