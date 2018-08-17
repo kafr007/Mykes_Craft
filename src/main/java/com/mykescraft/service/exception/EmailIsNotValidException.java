bf10a839a86e8c54c78f2ade0b51fbc4e8710e99
kafr007 <kafr007@gmail.com>
2018-08-17 22:43:14 +0200

    adding new exceptions which have to be send to the front end, when the error occurs

---
+++ src/main/java/com/mykescraft/service/exception/EmailIsNotValidException.java
@@ -0,0 +1,9 @@
+package com.mykescraft.service.exception;
+
+public class EmailIsNotValidException extends Exception {
+	
+	public EmailIsNotValidException(String message) {
+		super(message);
+	}
+
+}
---
+++ src/main/java/com/mykescraft/service/exception/FieldCanNotBeEmptyException.java
@@ -0,0 +1,9 @@
+package com.mykescraft.service.exception;
+
+public class FieldCanNotBeEmptyException extends Exception{
+	
+	public FieldCanNotBeEmptyException(String message){
+		super(message);
+	}
+
+}
