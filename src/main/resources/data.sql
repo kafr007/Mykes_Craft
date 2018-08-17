ef36ba207b5cd17eb26cbb74ae452f2e1a276a10
kafr007 <kafr007@gmail.com>
2018-08-17 23:31:58 +0200

    led url címek hozzáadása

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,10 +8,10 @@ insert into accessories(name, price, image_url, accessory_type)
 values ('Kristalykamra', 550, 'https://i.imgur.com/lnRZSUw.png','hilt');
 
 insert into accessories(name, price, color, image_url, accessory_type)
-values ('Red', 10, 'red', '', 'led');
+values ('Red', 10, 'red', 'https://i.imgur.com/KV8V36e.jpg', 'led');
 
 insert into accessories(name, price, color, image_url, accessory_type)
-values ('Blue', 15, 'blue', '', 'led');
+values ('Blue', 15, 'blue', 'https://i.imgur.com/gw5XZux.jpg', 'led');
 
 insert into accessories(name, price, color, image_url,accessory_type)
-values ('White', 12, 'white', '', 'led');
+values ('White', 12, 'white', 'https://i.imgur.com/Zl8tDa3.jpg', 'led');
