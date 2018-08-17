a96af401e55697f8859650ed48c42d344af84973
kafr007 <kafr007@gmail.com>
2018-08-17 22:46:11 +0200

    uncomment a log info

--- src/main/java/com/mykescraft/serviceimpl/ShoppingCartImpl.java
+++ src/main/java/com/mykescraft/serviceimpl/ShoppingCartImpl.java
@@ -21,7 +21,7 @@ import com.mykescraft.service.exception.AccessoryTypeAlreadyInTheCartException;
 
 @Service
 public class ShoppingCartImpl implements ShoppingCart {
-	
+
 	private final Logger log = LoggerFactory.getLogger(this.getClass());
 
 	@Autowired
@@ -29,7 +29,7 @@ public class ShoppingCartImpl implements ShoppingCart {
 
 	@Autowired
 	private AccessoryRepositoryImpl accessoryRepo;
-	
+
 	@Autowired
 	private CustomerRepositoryImpl customerRepo;
 
@@ -42,38 +42,37 @@ public class ShoppingCartImpl implements ShoppingCart {
 		amount = 0;
 	}
 
-	
 	@Override
-	public void addProductToCart(Accessory accessory){
+	public void addProductToCart(Accessory accessory) {
 		list.add(accessory);
 		amount += accessory.getPrice();
-			
+
 	}
-	
+
 	@Override
 	public boolean isAccessoryTypeAlreadyInTheCart(Accessory accessory) {
 		int counter = 0;
 		for (int i = 0; i < list.size(); i++) {
-			if(list.get(i).getClass().equals(accessory.getClass()))
+			if (list.get(i).getClass().equals(accessory.getClass()))
 				counter++;
 		}
-		
-		return counter!=0;
-		
+
+		return counter != 0;
+
 	}
 
 	@Override
 	public void removeProductFromCart(Accessory accessory) {
 		list.remove(accessory);
-		amount -=accessory.getPrice();
+		amount -= accessory.getPrice();
 	}
-	
+
 	@Override
 	public void save(Customer customer) {
 		log.info(customer.toString());
 		Order order = new Order();
 		order.setId(UUID.randomUUID().toString());
-		log.info("id" + order.getId());
+		//log.info("id" + order.getId());
 		customer.setId(UUID.randomUUID().toString());
 		customerRepo.saveCustomerData(customer);
 		order.setAmount(amount);
@@ -82,7 +81,7 @@ public class ShoppingCartImpl implements ShoppingCart {
 		order.setAccessories(list);
 		orderRepo.saveOrder(order);
 	}
-	
+
 	public ArrayList<Accessory> getList() {
 		return list;
 	}
