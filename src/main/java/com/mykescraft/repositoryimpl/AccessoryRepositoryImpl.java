953602f04902b83498e02f77e4ac693a05304495
kafr007 <kafr007@gmail.com>
2018-08-19 16:16:17 +0200

    h2 shema install

--- src/main/java/com/mykescraft/repositoryimpl/AccessoryRepositoryImpl.java
+++ src/main/java/com/mykescraft/repositoryimpl/AccessoryRepositoryImpl.java
@@ -25,15 +25,36 @@ public class AccessoryRepositoryImpl implements AccessoryRepository {
 	@Autowired(required=true)
 	private JdbcTemplate jdbcTemplate;
 	
+	private final String ID = "id";
+	private final String NAME = "name";
+	private final String PRICE = "price";
+	private final String IMAGEURL = "image_url";
+	private final String LENGTH = "length";
+	private final String TIP = "tip";
+	private final String WALLTHICKNESS = "walthickness";
+	private final String COLOR = "color";
+	
+	private final String FINDALLBLADESQL = "select id, name, price, image_url, tip, length, wallthickness from accessories where accessory_type='blade'";
+	private final String FINDALLBUTTONSQL = "select id, name, price, image_url from accessories where accessory_type='button'";
+	private final String FINDALLHILTSQL = "select id, name, price, image_url from accessories where accessory_type='hilt'";
+	private final String FINDALLLEDSQL = "select id, name, price, image_url, color from accessories where accessory_type='led'";
+	private final String FINDALLSOUNDCARDSQL = "select id, name, price, image_url from accessories where accessory_type='soundcard'";
+	
+	private final String FINDBLADEBYIDSQL = "select id, name, price, image_url, tip, length, wallthickness from accessories WHERE ID = ?";
+	private final String FINDBUTTONBYIDSQL = "select id, name, price, image_url from accessories WHERE ID = ?";
+	private final String FINDHILTBYIDSQL = "select id, name, price, image_url from accessories WHERE ID = ?";
+	private final String FINDLEDBYIDSQL = "select id, name, price, image_url, color from accessories WHERE ID = ?";
+	private final String FINDSOUNDCARDBYIDSQL = "select id, name, price, image_url from accessories WHERE ID = ?";
+	
 	private final RowMapper<Hilt> hiltMapper = new RowMapper<Hilt>() {
 
 		@Override
 		public Hilt mapRow(ResultSet rs, int rowNum) throws SQLException {
 			Hilt hilt = new Hilt();
-			hilt.setId(rs.getLong("id"));
-			hilt.setName(rs.getString("name"));
-			hilt.setPrice(rs.getDouble("price"));
-			hilt.setImageUrl(rs.getString("image_url"));
+			hilt.setId(rs.getLong(ID));
+			hilt.setName(rs.getString(NAME));
+			hilt.setPrice(rs.getDouble(PRICE));
+			hilt.setImageUrl(rs.getString(IMAGEURL));
 			return hilt;
 		}
 		
@@ -44,10 +65,10 @@ public class AccessoryRepositoryImpl implements AccessoryRepository {
 		@Override
 		public Button mapRow(ResultSet rs, int rowNum) throws SQLException {
 			Button button = new Button();
-			button.setId(rs.getLong("id"));
-			button.setName(rs.getString("name"));
-			button.setPrice(rs.getDouble("price"));
-			button.setImageUrl(rs.getString("image_url"));
+			button.setId(rs.getLong(ID));
+			button.setName(rs.getString(NAME));
+			button.setPrice(rs.getDouble(PRICE));
+			button.setImageUrl(rs.getString(IMAGEURL));
 			return button;
 		}
 		
@@ -58,13 +79,13 @@ public class AccessoryRepositoryImpl implements AccessoryRepository {
 		@Override
 		public Blade mapRow(ResultSet rs, int rowNum) throws SQLException {
 			Blade blade = new Blade();
-			blade.setId(rs.getLong("id"));
-			blade.setName(rs.getString("name"));
-			blade.setPrice(rs.getDouble("price"));
-			blade.setImageUrl(rs.getString("image_url"));
-			blade.setLength(rs.getDouble("length"));
-			blade.setTip(rs.getString("tip"));
-			blade.setWallThickness(rs.getString("walthickness"));
+			blade.setId(rs.getLong(ID));
+			blade.setName(rs.getString(NAME));
+			blade.setPrice(rs.getDouble(PRICE));
+			blade.setImageUrl(rs.getString(IMAGEURL));
+			blade.setLength(rs.getDouble(LENGTH));
+			blade.setTip(rs.getString(TIP));
+			blade.setWallThickness(rs.getString(WALLTHICKNESS));
 			return blade;
 		}
 		
@@ -75,11 +96,11 @@ public class AccessoryRepositoryImpl implements AccessoryRepository {
 		@Override
 		public Led mapRow(ResultSet rs, int rowNum) throws SQLException {
 			Led led = new Led();
-			led.setId(rs.getLong("id"));
-			led.setName(rs.getString("name"));
-			led.setPrice(rs.getDouble("price"));
-			led.setImageUrl(rs.getString("image_url"));
-			led.setColor(rs.getString("color"));
+			led.setId(rs.getLong(ID));
+			led.setName(rs.getString(NAME));
+			led.setPrice(rs.getDouble(PRICE));
+			led.setImageUrl(rs.getString(IMAGEURL));
+			led.setColor(rs.getString(COLOR));
 			return led;
 		}
 		
@@ -90,10 +111,10 @@ public class AccessoryRepositoryImpl implements AccessoryRepository {
 		@Override
 		public Soundcard mapRow(ResultSet rs, int rowNum) throws SQLException {
 			Soundcard soundcard = new Soundcard();
-			soundcard.setId(rs.getLong("id"));
-			soundcard.setName(rs.getString("name"));
-			soundcard.setPrice(rs.getDouble("price"));
-			soundcard.setImageUrl(rs.getString("image_url"));
+			soundcard.setId(rs.getLong(ID));
+			soundcard.setName(rs.getString(NAME));
+			soundcard.setPrice(rs.getDouble(PRICE));
+			soundcard.setImageUrl(rs.getString(IMAGEURL));
 			return soundcard;
 		}
 		
@@ -102,65 +123,60 @@ public class AccessoryRepositoryImpl implements AccessoryRepository {
 	@Override
 	public ArrayList<Blade> findAllBlades() {
 		ArrayList<Blade> list = (ArrayList<Blade>) this.jdbcTemplate.query(
-				"select id, name, price, image_url, tip, length, wallthickness from accessories where accessory_type='blade'",bladeMapper);
+				FINDALLBLADESQL,bladeMapper);
 		return list;
 	}
 
 	@Override
 	public ArrayList<Button> findAllButtons() {
 		ArrayList<Button> list = (ArrayList<Button>) this.jdbcTemplate.query(
-				"select id, name, price, image_url from accessories where accessory_type='button'", buttonMapper);
+				FINDALLBUTTONSQL, buttonMapper);
 		return list;
 	}
 
 	@Override
 	public ArrayList<Hilt> findAllHilts() {
 		ArrayList<Hilt> list = (ArrayList<Hilt>) this.jdbcTemplate.query(
-				"select id, name, price, image_url from accessories where accessory_type='hilt'", hiltMapper);
+				FINDALLHILTSQL, hiltMapper);
 		return list;
 	}
 
 	@Override
 	public ArrayList<Led> findAllLeds() {
 		ArrayList<Led> list = (ArrayList<Led>) this.jdbcTemplate.query(
-				"select id, name, price, image_url, color from accessories where accessory_type='led'", ledMapper);
+				FINDALLLEDSQL, ledMapper);
 		return list;
 	}
 
 	@Override
 	public ArrayList<Soundcard> findAllSoundcards() {
 		ArrayList<Soundcard> list = (ArrayList<Soundcard>) this.jdbcTemplate.query(
-				"select id, name, price, image_url from accessories where accessory_type='soundcard'", soundcardMapper);
+				FINDALLSOUNDCARDSQL, soundcardMapper);
 		return list;
 	}
 
 	@Override
 	public Blade findBladeById(long id) {
-		String sql = "select id, name, price, image_url, tip, length, wallthickness from accessories WHERE ID = ?";
-		return (Blade) jdbcTemplate.queryForObject(sql, bladeMapper, id);
+		return (Blade) jdbcTemplate.queryForObject(FINDBLADEBYIDSQL, bladeMapper, id);
 	}
 
 	@Override
 	public Button findButtonById(long id) {
-		String sql = "select id, name, price, image_url from accessories WHERE ID = ?";
-		return (Button) jdbcTemplate.queryForObject(sql, buttonMapper, id);
+		return (Button) jdbcTemplate.queryForObject(FINDBUTTONBYIDSQL, buttonMapper, id);
 	}
 	
 	public Hilt findHiltById(long id) {
-		String sql = "select id, name, price, image_url from accessories WHERE ID = ?";
-		return (Hilt) jdbcTemplate.queryForObject(sql, hiltMapper, id);
+		return (Hilt) jdbcTemplate.queryForObject(FINDHILTBYIDSQL, hiltMapper, id);
 	}
 
 	@Override
 	public Led findLedById(long id) {
-		String sql = "select id, name, price, image_url, color from accessories WHERE ID = ?";
-		return (Led) jdbcTemplate.queryForObject(sql, ledMapper, id);
+		return (Led) jdbcTemplate.queryForObject(FINDLEDBYIDSQL, ledMapper, id);
 	}
 
 	@Override
 	public Soundcard findSOundcardById(long id) {
-		String sql = "select id, name, price, image_url from accessories WHERE ID = ?";
-		return (Soundcard) jdbcTemplate.queryForObject(sql, soundcardMapper, id);
+		return (Soundcard) jdbcTemplate.queryForObject(FINDSOUNDCARDBYIDSQL, soundcardMapper, id);
 	}
 	
 	
